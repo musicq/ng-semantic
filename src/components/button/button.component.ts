7e5d444d519c5bfd7e0862c80ff8c65d9b330aac
musicq <liang.kui@hotmail.com>
2018-04-05 00:19:51 +0800

    feat(button): remove style scope

--- src/components/button/button.component.ts
+++ src/components/button/button.component.ts
@@ -1,13 +1,27 @@
-import { Component, Input, HostBinding, ElementRef } from '@angular/core';
+import { Component, ElementRef, Input, ViewEncapsulation } from '@angular/core';
 
 type COLOR_PATTERN = 'primary' | 'secondary' | '';
+
+const BUTTON_HOST_ATTRIBUTES = [
+  'se-button'
+];
 
 @Component({
   selector: '[se-button]',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.scss']
+  styleUrls: ['./button.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ButtonComponent {
+
+  constructor(private elementRef: ElementRef) {
+    for (const attr of BUTTON_HOST_ATTRIBUTES) {
+      if (this._hasHostAttribute(attr)) {
+        this.elementRef.nativeElement.classList.add(attr);
+      }
+    }
+  }
+
   private _type: COLOR_PATTERN;
 
   @Input()
@@ -16,15 +30,21 @@ export class ButtonComponent {
 
     if (value !== this._type) {
       if (this._type) {
-        (this.elementRef.nativeElement as HTMLElement).classList.remove(this._type);
+        this.hostElement.classList.remove(this._type);
       }
       if (value) {
-        (this.elementRef.nativeElement as HTMLElement).classList.add(value);
+        this.hostElement.classList.add(value);
       }
 
       this._type = value;
     }
   }
 
-  constructor(private elementRef: ElementRef) {}
+  private get hostElement(): HTMLElement {
+    return this.elementRef.nativeElement;
+  }
+
+  private _hasHostAttribute(attr: string) {
+    return this.hostElement.hasAttribute(attr);
+  }
 }
