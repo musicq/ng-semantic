a73bc8ae3d6e76b0cab57befd1d58baa772f171b
musicq <liang.kui@hotmail.com>
2018-04-04 23:23:21 +0800

    feat(button): refactor button component
    Use attr instead of element selector, this could make style override
    more easy

---
+++ src/components/button-group/button-group.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'se-button-group',
+  templateUrl: './button-group.component.html',
+  styleUrls: ['./button-group.scss']
+})
+export class ButtonGroupComponent {}
--- src/components/button/button.component.ts
+++ src/components/button/button.component.ts
@@ -1,10 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostBinding, ElementRef } from '@angular/core';
+
+type COLOR_PATTERN = 'primary' | 'secondary' | '';
 
 @Component({
-  selector: 'se-button',
+  selector: '[se-button]',
   templateUrl: './button.component.html',
   styleUrls: ['./button.scss']
 })
 export class ButtonComponent {
-  @Input() type = '';
+  private _type: COLOR_PATTERN;
+
+  @Input()
+  set type(value: COLOR_PATTERN) {
+    value = value || '';
+
+    if (value !== this._type) {
+      if (this._type) {
+        (this.elementRef.nativeElement as HTMLElement).classList.remove(this._type);
+      }
+      if (value) {
+        (this.elementRef.nativeElement as HTMLElement).classList.add(value);
+      }
+
+      this._type = value;
+    }
+  }
+
+  constructor(private elementRef: ElementRef) {}
 }
